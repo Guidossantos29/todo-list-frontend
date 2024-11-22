10fdcee40457fce26a6ce38991f27857c50921de
Luiz Guilherme <lguidossantos29@gmail.com>
2024-11-21 23:38:56 -0300

    feat: create globla style and HeaderComponent

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Header } from './style'
+
+function HeaderComponent() {
+    return (
+        <>
+            <Header>
+                <h1>Lista de Tarefas</h1>
+            </Header>
+        </>
+    )
+}
+
+export default HeaderComponent
---
+++ src/components/Header/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+
+export const Header = styled.header`
+    display: flex;  
+    justify-content: center;
+    background-color:#00008b ;
+    color: #FAF9F6;
+    padding: 20px;
+
+`
