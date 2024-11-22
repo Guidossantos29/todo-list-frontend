ecc536745b16054cf78d28d6a75b4f379cf9ac74
Luiz Guilherme <lguidossantos29@gmail.com>
2024-11-22 18:09:30 -0300

    feat: create api config

---
+++ src/api/api.jsx
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL:process.env.TODO_LIST_API_URL,
+})
+
+export const getTarefas = api.get("/tarefas")
+export const createTarefas = api.post("/tarefas",data)
+export const updateTarefas = api.put(`/tarefas/${id}`,data)
+export const deleteTarefas = api.delete(`/tarefas/${id}`)
+
+export default api
+
